193eb987d07ea5d6265f9712270a78dda196c8de
letsgojonny <letsgojonny@github.io>
2022-12-14 20:31:43 +0900

    15 puzzle

--- 15-puzzle practice/js/puzzle.js
+++ 15-puzzle practice/js/puzzle.js
@@ -1,257 +1,176 @@
+(function () {
+	var puzzle = document.getElementById('puzzle');
 
-var puzzle = document.getElementById('puzzle');
-var reset = document.getElementById('reset');
-var mix = document.getElementById('mix');
+	solve();
 
-// Listens for click on each buttons
+	function solve() {
+		puzzle.innerHTML = '';
+		var n = 1;
+		for (var i = 0; i <= 3; i++) {
+			for (var j = 0; j <= 3; j++) {
+				var box = document.createElement('div');
+				box.id = 'box-' + i + '-' + j;
+				box.style.top = (i * 9 + 1 * i + 1) + 'rem';
+				box.style.left = (j * 9 + 1 * j + 1) + 'rem';
 
-document.getElementById('puzzle').addEventListener('click', puzzle);
-document.getElementById('controls').addEventListener('click', controls);
-document.getElementById('reset').addEventListener('click', reset);
-document.getElementById('mix').addEventListener('click', mix);
-
-// mix puzzle
-
-var state = 1;
-
-
-// Creates solved puzzle
-solve();
-
-// Listens for click on puzzle cells
-
-function mix() {
-	puzzle.addEventListener('click', function (e) {
-		if (state == 1) {
-			// Enables sliding animation
-			puzzle.className = 'animate';
-			shiftCell(e.target);
-		}
-	});
-}
-
-function shiftCell(cell) {
-
-	// Checks if selected cell has number
-	if (cell.clasName != 'empty') {
-
-		// Tries to get empty adjacent cell
-		var emptyCell = getEmptyAdjacentCell(cell);
-
-		if (emptyCell) {
-			// Temporary data
-			var tmp = { style: cell.style.cssText, id: cell.id };
-
-			// Exchanges id and style values
-			cell.style.cssText = emptyCell.style.cssText;
-			cell.id = emptyCell.id;
-			emptyCell.style.cssText = tmp.style;
-			emptyCell.id = tmp.id;
-
-			if (state == 1) {
-				// Checks the order of numbers
-				setTimeout(checkOrder, 150);
+				if (n <= 15) {
+					box.classList.add('number');
+					box.classList.add((i % 2 == 0 && j % 2 > 0 || i % 2 > 0 && j % 2 == 0) ? 'dark' : 'light');
+					box.innerHTML = (n++).toString();
+				} else {
+					box.className = 'empty';
+				}
+				puzzle.appendChild(box);
 			}
 		}
 	}
 
-}
+		// Listens for click on puzzle boxs
+	puzzle.addEventListener('click', function (e) {
+		// Enables sliding animation
+		puzzle.className = 'animate';
+		shiftbox(e.target);
+	});
+
+	// Listens for click on control buttons
+	document.getElementById('scramble').addEventListener('click', scramble);
+
+	// Shifts number box to the empty box
+
+	function shiftbox(box) {
+		// Checks if selected box has number
+		if (box.className != 'empty') {
+
+			// Tries to get empty adjacent box
+			var emptybox = getEmptyAdjacentbox(box);
+
+			if (emptybox) {
+				// Temporary data
+				var tmp = { style: box.style.cssText, id: box.id };
+
+				// Exchanges id and style values
+				box.style.cssText = emptybox.style.cssText;
+				box.id = emptybox.id;
+				emptybox.style.cssText = tmp.style;
+				emptybox.id = tmp.id;
+
+				// Checks the order of numbers
+				setTimeout(checkOrder, 150);
+			}
+		}
+
+	}
+	// Gets specific box by row and column
+	function getbox(row, col) {
+		return document.getElementById('box-' + row + '-' + col);
+	}
+
+	// Gets empty box
+
+	function getEmptybox() {
+		return puzzle.querySelector('.empty');
+	}
+
+	// Gets empty adjacent box if it exists
+	function getEmptyAdjacentbox(box) {
+
+		// Gets all adjacent boxs
+		var adjacent = getAdjacentboxs(box);
+
+		// Searches for empty box
+		for (var i = 0; i < adjacent.length; i++) {
+			if (adjacent[i].className == 'empty') {
+				return adjacent[i];
+			}
+		}
+
+		// Empty adjacent box was not found
+		return false;
+	}
 
 
-// 	if (state == 0) {
-// 		return;
-// 	}
+	// Gets all adjacent boxs
 
-// 	puzzle.removeAttribute('class');
-// 	state = 0;
+	function getAdjacentboxs(box) {
 
-// 	var previousCell;
-// 	var i = 1;
-// 	var interval = setInterval(function () {
-// 		if (i <= 100) {
-// 			var adjacent = getAdjacentCells(getEmptyCell());
-// 			if (previousCell) {
-// 				for (var j = adjacent.length - 1; j >= 0; j--) {
-// 					if (adjacent[j].innerHTML == previousCell.innerHTML) {
-// 						adjacent.splice(j, 1);
-// 					}
-// 				}
-// 			}
-// 			// Gets random adjacent cell and memorizes it for the next iteration
-// 			previousCell = adjacent[rand(0, adjacent.length - 1)];
-// 			shiftCell(previousCell);
-// 			i++;
-// 		} else {
-// 			clearInterval(interval);
-// 			state = 1;
-// 		}
-// 	}, 5);
+		var id = box.id.split('-');
 
-// }
+		// Gets box position indexes
+		var row = parseInt(id[1]);
+		var col = parseInt(id[2]);
 
+		var adjacent = [];
 
+		// Gets all possible adjacent boxs
+		if (row < 3) { adjacent.push(getbox(row + 1, col)); }
+		if (row > 0) { adjacent.push(getbox(row - 1, col)); }
+		if (col < 3) { adjacent.push(getbox(row, col + 1)); }
+		if (col > 0) { adjacent.push(getbox(row, col - 1)); }
 
-// (function () {
+		return adjacent;
+	}
 
-// 	var state = 1;
-// 	var puzzle = document.getElementById('puzzle');
+	// Checks if the order of numbers is correct
 
-// 	// Creates solved puzzle
-// 	solve();
+	function checkOrder() {
 
-// 	// Listens for click on puzzle cells
-// 	puzzle.addEventListener('click', function (e) {
-// 		if (state == 1) {
-// 			// Enables sliding animation
-// 			puzzle.className = 'animate';
-// 			shiftCell(e.target);
-// 		}
-// 	});
+		// Checks if the empty box is in correct position
+		if (getbox(3, 3).className != 'empty') {
+			return;
+		}
 
-// 	/**
-// 	 * Creates solved puzzle
-// 	 *
-// 	 */
-// 	function solve() {
+		var n = 1;
+		// Goes through all boxs and checks numbers
+		for (var i = 0; i <= 3; i++) {
+			for (var j = 0; j <= 3; j++) {
+				if (n <= 15 && getbox(i, j).innerHTML != n.toString()) {
+					// Order is not correct
+					return;
+				}
+				n++;
+			}
+		}
 
-// 		if (state == 0) {
-// 			return;
-// 		}
+		// Puzzle is solved, offers to scramble it
+		if (confirm('Congrats, You did it! \nScramble the puzzle?')) {
+			scramble();
+		}
+	}
 
-// 		puzzle.innerHTML = '';
+	// Scrambles puzzle
+	function scramble() {
+		puzzle.removeAttribute('class');
 
-// 		var n = 1;
-// 		for (var i = 0; i <= 3; i++) {
-// 			for (var j = 0; j <= 3; j++) {
-// 				var cell = document.createElement('span');
-// 				cell.id = 'cell-' + i + '-' + j;
-// 				cell.style.left = (j * 80 + 1 * j + 1) + 'px';
-// 				cell.style.top = (i * 80 + 1 * i + 1) + 'px';
-
-// 				if (n <= 15) {
-// 					cell.classList.add('number');
-// 					cell.classList.add((i % 2 == 0 && j % 2 > 0 || i % 2 > 0 && j % 2 == 0) ? 'dark' : 'light');
-// 					cell.innerHTML = (n++).toString();
-// 				} else {
-// 					cell.className = 'empty';
-// 				}
-
-// 				puzzle.appendChild(cell);
-// 			}
-// 		}
-
-// 	}
-
-	/**
-	 * Shifts number cell to the empty cell
-	 *
-	 */
+		var previousbox;
+		var i = 1;
+		var interval = setInterval(function () {
+			if (i <= 100) {
+				var adjacent = getAdjacentboxs(getEmptybox());
+				if (previousbox) {
+					for (var j = adjacent.length - 1; j >= 0; j--) {
+						if (adjacent[j].innerHTML == previousbox.innerHTML) {
+							adjacent.splice(j, 1);
+						}
+					}
+				}
+				// Gets random adjacent box and memorizes it for the next iteration
+				previousbox = adjacent[rand(0, adjacent.length - 1)];
+				shiftbox(previousbox);
+				i++;
+			} else {
+				clearInterval(interval);
+			}
+		}, 5);
+	}
 	
+	// Generates random number
+	function rand(from, to) {
+		return Math.floor(Math.random() * (to - from + 1)) + from;
+	}
 
-// 	/**
-// 	 * Gets specific cell by row and column
-// 	 *
-// 	 */
-// 	function getCell(row, col) {
-
-// 		return document.getElementById('cell-' + row + '-' + col);
-
-// 	}
-
-// 	/**
-// 	 * Gets empty cell
-// 	 *
-// 	 */
-// 	function getEmptyCell() {
-
-// 		return puzzle.querySelector('.empty');
-
-// 	}
-
-// 	/**
-// 	 * Gets empty adjacent cell if it exists
-// 	 *
-// 	 */
-// 	function getEmptyAdjacentCell(cell) {
-
-// 		// Gets all adjacent cells
-// 		var adjacent = getAdjacentCells(cell);
-
-// 		// Searches for empty cell
-// 		for (var i = 0; i < adjacent.length; i++) {
-// 			if (adjacent[i].className == 'empty') {
-// 				return adjacent[i];
-// 			}
-// 		}
-
-// 		// Empty adjacent cell was not found
-// 		return false;
-
-// 	}
-
-// 	/**
-// 	 * Gets all adjacent cells
-// 	 *
-// 	 */
-// 	function getAdjacentCells(cell) {
-
-// 		var id = cell.id.split('-');
-
-// 		// Gets cell position indexes
-// 		var row = parseInt(id[1]);
-// 		var col = parseInt(id[2]);
-
-// 		var adjacent = [];
-
-// 		// Gets all possible adjacent cells
-// 		if (row < 3) { adjacent.push(getCell(row + 1, col)); }
-// 		if (row > 0) { adjacent.push(getCell(row - 1, col)); }
-// 		if (col < 3) { adjacent.push(getCell(row, col + 1)); }
-// 		if (col > 0) { adjacent.push(getCell(row, col - 1)); }
-
-// 		return adjacent;
-
-// 	}
-
-// 	/**
-// 	 * Chechs if the order of numbers is correct
-// 	 *
-// 	 */
-// 	function checkOrder() {
-
-// 		// Checks if the empty cell is in correct position
-// 		if (getCell(3, 3).className != 'empty') {
-// 			return;
-// 		}
-
-// 		var n = 1;
-// 		// Goes through all cells and checks numbers
-// 		for (var i = 0; i <= 3; i++) {
-// 			for (var j = 0; j <= 3; j++) {
-// 				if (n <= 15 && getCell(i, j).innerHTML != n.toString()) {
-// 					// Order is not correct
-// 					return;
-// 				}
-// 				n++;
-// 			}
-// 		}
-
-// 		// Puzzle is solved, offers to mix it
-// 		if (confirm('Congrats, You did it! \n mix the puzzle?')) {
-// 			mix();
-// 		}
-
-// 	}
-
-
-// 	/**
-// 	 * Generates random number
-// 	 *
-// 	 */
-// 	function rand(from, to) {
-
-// 		return Math.floor(Math.random() * (to - from + 1)) + from;
-
-// 	}
-
-// }());
+	reset = solve.bind();
+	reset(); 
+	
+	document.getElementById('reset').addEventListener('click', reset);
+	
+}());
